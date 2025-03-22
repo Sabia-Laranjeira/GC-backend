0ae300512e31a721692f8159789f7247ad68d3f9
Sabia-laranjeira <marqueslpa20@gmail.com>
2025-03-21 23:17:33 -0300

    Trying with some methods.

--- src/app.js
+++ src/app.js
@@ -5,13 +5,15 @@ import { configDotenv } from 'dotenv';
 import compraRouter from './routes/compraRouter.js';
 import produtosRouter from './routes/produtosRouter.js';
 import relatoriosRouter from './routes/relatoriosRouter.js';
+import { methods } from 'expres';
 
 configDotenv();
 const app = express();
 const url = process.env.URL
 
 const corsOption = {
-    "origin": "https://gc-frontend-mu.vercel.app/"
+    origin: "https://gc-frontend-mu.vercel.app/",
+    methods: ["GET,HEAD,PUT,PATCH,POST,DELETE",]
   }
 
 app.use(cors(corsOption));
