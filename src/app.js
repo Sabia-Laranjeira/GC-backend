ed28a100ada394a887971e48f9e747e9ea3d7032
Sabia-laranjeira <marqueslpa20@gmail.com>
2025-03-22 13:56:49 -0300

    [FIX]: Front-end communication.

--- src/app.js
+++ src/app.js
@@ -10,18 +10,18 @@ configDotenv();
 const app = express();
 const url = process.env.URL
 
-/*const corsOption = {
-    origin: "https://gc-frontend-mu.vercel.app",
-    methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"]
-  }*/
+const allowedOrigins = ["https://gc-backend-wp6d.onrender.com", "https://www.gc-backend-wp6d.onrender.com"];
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://gc-frontend-mu.vercel.app");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header("Access-Control-Allow-Origin", origin);
+    }
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
-//app.use(cors(corsOption));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
