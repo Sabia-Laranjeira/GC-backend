da04b5bfd28ecc8a1baf42779ed9f38d44ab9c8f
Sabia-laranjeira <marqueslpa20@gmail.com>
2025-03-21 23:25:00 -0300

    Changed the middleware.

--- src/app.js
+++ src/app.js
@@ -5,18 +5,23 @@ import { configDotenv } from 'dotenv';
 import compraRouter from './routes/compraRouter.js';
 import produtosRouter from './routes/produtosRouter.js';
 import relatoriosRouter from './routes/relatoriosRouter.js';
-import { methods } from 'expres';
 
 configDotenv();
 const app = express();
 const url = process.env.URL
 
-const corsOption = {
+/*const corsOption = {
     origin: "https://gc-frontend-mu.vercel.app",
-    methods: ["GET,HEAD,PUT,PATCH,POST,DELETE",]
-  }
+    methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"]
+  }*/
 
-app.use(cors(corsOption));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://gc-frontend-mu.vercel.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+//app.use(cors(corsOption));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
