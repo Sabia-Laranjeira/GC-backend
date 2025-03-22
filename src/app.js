3ad0fff4e2a7ec811fbdc9be905e002815a6bce4
Sabia-laranjeira <marqueslpa20@gmail.com>
2025-03-21 21:43:08 -0300

    Trying to fix CORS part 1.

--- src/app.js
+++ src/app.js
@@ -9,22 +9,12 @@ import relatoriosRouter from './routes/relatoriosRouter.js';
 configDotenv();
 const app = express();
 const url = process.env.URL
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-app.use((req,res,next) => {
-  res.setHeader("Access-Control-Allow-Origin", `${url}`);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-})
 
 app.use(cors({
   origin: `${url}`,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH']
 }));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use(compraRouter);
 app.use(produtosRouter);
