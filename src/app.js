2a7b13c5c89ac5c112e2ea690fb0a4fd2ee199d1
Sabia-laranjeira <marqueslpa20@gmail.com>
2025-03-23 22:11:36 -0300

    .

--- src/app.js
+++ src/app.js
@@ -23,10 +23,9 @@ app.use((req, res, next) => {
         res.header("Access-Control-Allow-Origin", origin);
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-
         return next();
     } 
-
+    
     return res.status(403).json({ error: "Acesso negado" });
 });
 
